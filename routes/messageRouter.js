13f611dd38a78b518a82db92eb4c4b3dc15edb3f
Brendan <brendanlawless@hotmail.com>
2024-11-06 12:42:55 +0000

    feat: add view for individual message

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,15 +1,37 @@
 import { Router } from "express";
+import messages from "../db.js"
+import pkg from "express-handlebars"
 
 const messageRouter = Router();
 
+const create = pkg.create
+const hbs = create({
+    helpers: {
+        message(id) {
+            let string = '';
+            const { user, text, added} = messages[id];
+            string+=`${user} ${text} ${added}`;
+            return string;
+        }
+    }
+})
+
 messageRouter.post("/", (req, res) => {
     const {user, message} = req.body
+    messages.push({user: user, text: message, added: new Date() })
     res.redirect("/")
 })
+
+messageRouter.get("/:id", (req, res) => {
+    console.log(req.params)
+    const { id } = req.params;
+    res.render("message", {layout: "index", helpers: { post: hbs.helpers.message(id) }});
+});
 
 messageRouter.get("/", (req, res) => {
     res.render("form", {layout: "index"});
 });
+
 
 
 export default messageRouter;
