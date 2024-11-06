9bbf6d8c7396de23f43e440f10aa0880069de621
Brendan <brendanlawless@hotmail.com>
2024-11-06 12:40:27 +0000

    feat: pass form path to view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,34 +1,23 @@
 import { Router } from "express";
 import pkg from "express-handlebars"
+import messages from "../db.js"
 
 const indexRouter = Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
 
 const create = pkg.create
 const hbs = create({
     helpers: {
         val() {
             let string = ''
-            messages.map(message => string+=`<p>${message.text} ${message.user} ${message.added}</p>`)
+            messages.map((message, i) => string+=`<p>${message.text} ${message.user} ${message.added}<a href="new/${i}">open</a></p>`)
             return string;
         },
+    form() { return "/new"}
     }
 })
 
 indexRouter.get("/", (req, res) => {
-    res.render("main", {layout: "index", helpers: { messages: hbs.helpers.val() }});
+    res.render("main", {layout: "index", helpers: { messages: hbs.helpers.val(), form: hbs.helpers.form()}});
 });
 
 export default indexRouter;
